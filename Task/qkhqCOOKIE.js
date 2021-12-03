fa6fbfcd65cfb69cbab315f5ca4f77b2a01ecfdd
a1564471347 <65760851+a1564471347@users.noreply.github.com>
2021-12-03 18:46:18 +0800

    Update qkhqCOOKIE.js

--- Task/qkhqCOOKIE.js
+++ Task/qkhqCOOKIE.js
@@ -12,7 +12,7 @@ module.exports = {
     }, {
       "id": "qkhqCount",
       "name": "账号个数",
-      "val": "1",
+      "val": "3",
       "type": "number",
       "desc": "指定任务最多跑几个账号，根据抓取的账号数据个数来设值"
     }, {
@@ -60,13 +60,13 @@ module.exports = {
     "favIconColor": "grey",
     "datas": [{
       "key": "qkhqheader",
-      "val": ""
+      "val": "U9zxwd3rizFP-HFW1IUtiJnotKizD9a9j1ETl5oEL69_g7lqn0AbkTKPq046dz8BXC-SfBRHlJ58FzQ8FErFyAnnXF0aBKI9QqEsWSkbqPUwJUX9ALaXyRezQhH-ZrwuL_jDYhp28MSHR_QZj7nRCAJLrRH8p7A9w9RV3PkdUV9pXpDr1Y9mmY3lhJZ07yB8Stc5R3x02RiFOn0AayFoqc-sT-hQr0gh1uNGSpprrR_t0mDI9pVWisYijV6AeLbL97sb3ueGs-TpBdx2RNzsSbDrmRbEB1T1EDmsQxtzE9KMR-vvBUDhi_wzcxGV3TIgcB_a2jkE3DNRg4q8NeMJqA=="
     }, {
       "key": "qkhqheader2",
-      "val": ""
+      "val": "oJ6Xn_kkfbXlFm4nM2hIBT-XRTqdqebfgq3LJ2nuV6nfX5yAt56gRrYVnF2udLav3JS8LaPDoNzCxOVsN-GxhAsCFTte14Xhp9wSgkZswjTdLjooiXv-eop1_NMtXHPIDwQpwo-lguSw7M3Gc6N6rmKJpW0vkG9ZmeD7d0rLhXECVCQTx2TyV6GIvQ9T1r3xB1PzSValx6HEJEyfPeOB5rkkfPYoLwSzrGzj6nWmT9pFxccEAgq3_-w6GUlW6PPYCcRiz_lcO0_sIYzk3D3Q40IQcg46hI2cqnuwUk1DP_F8h4Qm9jU3DYwcDy9tKoF11rI5eTOn-lhjqSCkpWYLTA=="
     }, {
       "key": "qkhqheader3",
-      "val": ""
+      "val": "o2Ao29LMDpcsM1w1sxgIry2DR-aBd0ur7FpvljHgxkXA7HAGPHmuBRAj_4i3LzCKQJBEG2hlRRs_oXNv4eU0HY6f316u_okmHqLQpdjFzycmFQdf2Eauq3970-qwq29e6tcOW4zyt4eouo5_ui0xFzCAo3EHxrBZFec9ng2N0DDTulEP4b3435ChmIvvnB8n9XEBfT7eunO3K8oEl7hb5z-9i3-TzZ1krP88GGmqms-38AzEROjADwRbftGgyeSyCQItkzqEwpEW58aEnNKkrGOKTBmqB9OGuoSVpjqXMzC9WxqwoLsGuwH0aAOgE9gHjVodvbAYtH7JFGapNoZswA=="
     }, {
       "key": "qkhqheader4",
       "val": ""
